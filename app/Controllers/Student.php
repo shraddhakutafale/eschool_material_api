0bf3438c3490572942c7b4e94d64f573fdaa32aa
Praful Sable <praful111sable@gmail.com>
2025-01-29 16:29:40 +0530

    Student

---
+++ app/Controllers/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Student extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
